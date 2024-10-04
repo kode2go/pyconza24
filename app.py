258e9f3076a34932b870097c99afbd8dc97872dd
kode2go <brsbin001@myuct.ac.za>
2024-10-04 10:16:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Add a title to the sidebar
-st.sidebar.title("PyConZA 2024")
+st.sidebar.title("PyConZA 2024 - Streamlit")
 
 # Sidebar menu for navigation
 page = st.sidebar.selectbox(
