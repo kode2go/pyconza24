83a013651ef35960e2ff1cc5eed15085ff82ee52
kode2go <brsbin001@myuct.ac.za>
2024-10-04 08:09:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.write("PyconZA 2024")
+# Read the CSV
+df = pd.read_csv('talks.csv')
+
+# Create separate tabs for different categories
+tab_names = ["Talk", "Remote Talk", "Keynote", "Tutorial", "Open Space", "Sponsor Talk"]
+tabs = st.tabs(tab_names)
+
+# Iterate through each tab and display the relevant data
+for i, tab in enumerate(tabs):
+    with tab:
+        category = tab_names[i]
+        st.write(f"## {category}")
+        filtered_df = df[df['Category'] == category]
+        st.dataframe(filtered_df)
